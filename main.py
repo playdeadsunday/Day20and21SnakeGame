94c816505b070b31446716c7e739f4c5fd0367e9
playdeadsunday <aniketc713@gmail.com>
2021-12-19 15:59:39 +0530

    0.1

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ screen.bgcolor("black")
 screen.title("My snake game.")
 
 x_pos = 0
+
+segments = []
 for _ in range(3):
-    new_turtle = Turtle("square")
-    new_turtle.goto(x_pos, 0)
-    new_turtle.color("white")
+    new_segment = Turtle("square")
+    new_segment.penup()
+    new_segment.goto(x_pos, 0)
+    new_segment.color("white")
+    segments.append(new_segment)
     x_pos -= 20
+
 
 screen.exitonclick()
